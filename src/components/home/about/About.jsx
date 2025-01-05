f064424d2010b7b1adfdd6707e575a90b20e2500
Palash Hawee <haweepalash@gmail.com>
2025-01-05 14:32:35 +0600

    added contact

--- src/components/home/about/About.jsx
+++ src/components/home/about/About.jsx
@@ -6,20 +6,31 @@ import HappyClients from "../HappyClient";
 import ContactUs from "../Contact";
 import Footer from "@/components/Footer";
 import VisionMissionValues from "@/components/visionMission/VisionMissionValues";
+import Contact from "@/components/contact/Contact";
 
 const AboutSection = () => {
   return (
-    <div>
-      <HeroSection />
-      <CardCon />
+    <>
+      <div>
+        <HeroSection />
+        <CardCon />
 
-      <div className="container mx-auto px-4 py-16">
-        <VisionMissionValues/>
+        <div className="container mx-auto px-4 py-16">
+          <VisionMissionValues />
+        </div>
+        <HappyClients />
+        {/* Contact Section */}
+        <div
+          id="contact-section"
+          className="min-h-screen bg-gradient-to-b from-customBlue-700 to-customBlue-800  flex items-center justify-center"
+        >
+          <div className="text-center px-6 sm:px-12">
+            <Contact />
+          </div>
+        </div>
+        <Footer />
       </div>
-      <HappyClients />
-      <ContactUs />
-      <Footer/>
-    </div>
+    </>
   );
 };
 
